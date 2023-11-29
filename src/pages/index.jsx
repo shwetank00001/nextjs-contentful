bae37204710f879750857079b038658d079c594f
shwetank00001 <mshwetank@gmail.com>
2023-11-29 16:15:17 +0530

    gg

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 const Homepage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Kindly select the Posts tab for the posts</h1>
+      <h1 className="text-3xl font-bold">I have made this using Nextjs and used Contentful for data management. Kindly select the Posts tab for the posts</h1>
     </div>
   );
 };
